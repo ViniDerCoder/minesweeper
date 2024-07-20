35196480ea92f9fca7888d89f5f1787d9da70ecf
ViniDerCoder <85809337+ViniDerCoder@users.noreply.github.com>
2024-07-20 17:13:18 +0200

    Automatic field clicking

--- Minesweeper.js
+++ Minesweeper.js
@@ -2,7 +2,9 @@ import { updateGrid } from "./index.js";
 
 
 export class Minesweeper {
-    //0 = grass, -1 = mine, 1 = flag, 2 = dirt
+    //-1 = grass, -2 = mine, -3 = flag, -4 = dirt, > 0 = dirt with n mines around
+
+    gameLocked = false;
 
     constructor(size, mines) {
         this.size = size;
@@ -16,7 +18,7 @@ export class Minesweeper {
         for (let i = 0; i < this.size; i++) {
             let row = [];
             for (let j = 0; j < this.size; j++) {
-                row.push(0);
+                row.push(-1);
             }
             board.push(row);
         }
@@ -46,17 +48,94 @@ export class Minesweeper {
 
     render() {
         updateGrid(this.board)
-        setTimeout(() => {
-            this.render()
-        }, 100)
     }
 
-    click(x, y) {
-        if (this.mineBoard[y][x]) {
-            this.board[y][x] = -1;
+    click(x, y, flag = false) {
+        if(x < 0 || x >= this.size || y < 0 || y >= this.size || this.board[y][x] === -4 || this.gameLocked) return
+        if (flag) {
+            if (this.board[y][x] === -3) {
+                this.board[y][x] = -1;
+            } else if (this.board[y][x] === -1) {
+                this.board[y][x] = -3;
+            }
         } else {
-            this.board[y][x] = 2;
+            if (this.mineBoard[y][x]) {
+                this.board[y][x] = -2;
+                this.gameLocked = true;
+                this.render();
+                this.endCallback ? this.endCallback("lose") : null;
+            } else {
+                const bombs = this.getFieldNumber(x, y);
+                if (bombs === 0) {
+                    this.board[y][x] = -4;
+                    setTimeout(() => {
+                        this.click(x - 1, y);
+                        this.click(x + 1, y);
+                        this.click(x, y - 1);
+                        this.click(x, y + 1);
+                        this.click(x - 1, y - 1);
+                        this.click(x + 1, y + 1);
+                        this.click(x + 1, y - 1);
+                        this.click(x - 1, y + 1);
+                    }, 300);
+                } else {
+                    this.board[y][x] = bombs;
+                }
+            }
+        }
+        this.render();
+        if(this.checkWin()) {
+            this.gameLocked = true;
+            this.endCallback ? this.endCallback("win") : null;
         }
     }
 
+    getFieldNumber(x, y) {
+        let count = 0;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (x + i >= 0 && x + i < this.size && y + j >= 0 && y + j < this.size) {
+                    if (this.mineBoard[y + j][x + i]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    get bombs() {
+        return this.mines;
+    }
+
+    get flags() {
+        let count = 0;
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (this.board[j][i] === -3) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    checkWin() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (this.board[j][i] === -1 && !this.mineBoard[j][i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * 
+     * @param {(state: "win" | "lose") => void} callback 
+     */
+    onEnd(callback) {
+        this.endCallback = callback;
+    }
 }
--- index.js
+++ index.js
@@ -8,7 +8,9 @@ gridElement.style.gridTemplateRows = 'repeat(' + gridWidth + ', 1fr)';
 gridElement.style.gridTemplateColumns = 'repeat(' + gridWidth + ', 1fr)';
 
 
-const minesweeper = new Minesweeper(gridWidth, 5);
+const minesweeper = new Minesweeper(gridWidth, 10);
+
+let flagMode = false;
 
 for (let i = 0; i < gridWidth * gridWidth; i++) {
     const gridItem = document.createElement('div');
@@ -17,12 +19,27 @@ for (let i = 0; i < gridWidth * gridWidth; i++) {
     gridItem.dataset.style = (i + Math.floor(i/gridWidth)) % 2 === 0 ? 'light' : 'dark';
     gridItem.addEventListener('click', () => {
         console.log('clicked ', Math.floor(i/gridWidth), i - Math.floor(i/gridWidth) * gridWidth);
-        minesweeper.click(i - Math.floor(i/gridWidth) * gridWidth, Math.floor(i/gridWidth));
+        minesweeper.click(i - Math.floor(i/gridWidth) * gridWidth, Math.floor(i/gridWidth), flagMode);
     });
     gridElement.appendChild(gridItem);
 }
 
+const flagButton = document.getElementById('flag-toggler');
+const flagCounter = document.getElementById('flag-counter');
+flagButton.addEventListener('click', () => {
+    flagMode = !flagMode;
+    flagButton.dataset.activated = flagMode;
+});
+
 minesweeper.render();
+
+minesweeper.onEnd((state) => {
+    if(state === 'win') {
+        alert('You won!');
+    } else {
+        alert('You lost!');
+    }
+})
 
 /**
  * @param {Array<Array<number>>} grid 
@@ -34,14 +51,22 @@ export function updateGrid(grid) {
             if(!gridItem) {
                 continue;
             }
-            if(grid[row][cell] === 1) {
+            if(grid[row][cell] === -3) {
                 gridItem.dataset.flag = true;
-            } else if(grid[row][cell] === 2) {
+            } else if(grid[row][cell] === -4) {
                 gridItem.dataset.bomb = false;
-            } else if(grid[row][cell] === -1) {
+            } else if(grid[row][cell] === -2) {
                 gridItem.dataset.bomb = true;
+            } else if(grid[row][cell] === -1) {
+                delete gridItem.dataset.bomb;
+                delete gridItem.dataset.flag;
+            } else {
+                gridItem.dataset.bomb = false;
+                gridItem.dataset.flag = false;
+                gridItem.innerText = grid[row][cell];
             }
 
         }
     }
+    flagCounter.innerText = minesweeper.bombs - minesweeper.flags;
 }
